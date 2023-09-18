db7f9a6f99f3e28d0bb27bff66f79ebc94a7c41d
xhx <jacksonxie612@gmail.com>
2023-09-18 08:28:17 +0800

    match server 3.0

--- matchSystem/src/main.cpp
+++ matchSystem/src/main.cpp
@@ -15,6 +15,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+#include <unistd.h>
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
@@ -57,12 +58,24 @@ class Pool {
 		}
 
 		void match() {
-			while (users.size() > 2) {
-				auto a = users[0], b = users[1];
-				users.erase(users.begin());
-				users.erase(users.begin());
+			while (users.size() > 1) {
+                sort(users.begin(), users.end(), [&](User& a, User b) {
+                        return a.score < b.score;
+                        });
 
-				save_result(a.id, b.id);
+                bool flag = true;
+                for (uint32_t i = 1; i < users.size(); i ++) {
+                    auto a = users[i - 1], b = users[i];
+                    if (b.score - a.score <= 50) {
+                        users.erase(users.begin() + i - 1, users.begin() + i + 1);
+                        save_result(a.id, b.id);
+
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag) break;
 			}
 		}
 
@@ -116,7 +129,10 @@ void consume_task(){
 	while(true) {
 		unique_lock<mutex> lck(message_queue.m);
 		if (message_queue.q.empty()) {
-			message_queue.cv.wait(lck);
+			//message_queue.cv.wait(lck);
+            lck.unlock();
+            pool.match();
+            sleep(1);
 		} else {
 			auto task = message_queue.q.front();
 			message_queue.q.pop();
